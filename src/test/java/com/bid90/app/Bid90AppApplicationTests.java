959095faf0bbcedba56ab9ab6f93fea6dd6eeb5e
BID90 <bradea.ioan.daniel@gmail.com>
2020-07-07 03:42:12 +0300

    test

--- src/test/java/com/bid90/app/Bid90AppApplicationTests.java
+++ src/test/java/com/bid90/app/Bid90AppApplicationTests.java
@@ -2,8 +2,10 @@ package com.bid90.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class Bid90AppApplicationTests {
 
 	@Test
